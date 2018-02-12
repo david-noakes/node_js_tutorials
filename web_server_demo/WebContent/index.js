2e5d24d71b44c7dd6eb705bebc5b39cdaf81a21d
david noakes <david.noakes@gmail.com>
2018-02-12 17:22:56 +1000

    missed this one

--- web_server_demo/WebContent/index.js
+++ web_server_demo/WebContent/index.js
@@ -3,7 +3,7 @@ console.log("Basic HTTP server");
 var http = require('http');
 
 var handlerMethod = function(req,res){
-	res.end("A simple response from the simple web seerver");
+	res.end("A simple response from the simple web server");
 }
 
 http.createServer(handlerMethod).listen(3456, 'localhost');
