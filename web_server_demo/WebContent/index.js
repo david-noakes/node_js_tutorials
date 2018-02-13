678febe79ea7fa5fb3fbfd51a284ed67a5247522
david noakes <david.noakes@gmail.com>
2018-02-13 10:38:59 +1000

    working with a POST request Note: form not reachable without using file:///D:/Warehouse/git_clones/node_js_tutorials/web_server_demo/pages/useForm.html

--- web_server_demo/WebContent/index.js
+++ web_server_demo/WebContent/index.js
@@ -1,10 +1,30 @@
-console.log("Basic HTTP server");
+console.log("Basic HTTP server v0.0.02 - Process POST request");
+/*
+ * change 0.0.02 dependencies in package.json require >npm install
+ * to use pages/userForm.html we need to enter
+ * file:///D:/Warehouse/git_clones/node_js_tutorials/web_server_demo/pages/userForm.html
+ */
 
 var http = require('http');
+var connect = require('connect');
+var bodyParser = require('body-parser');
 
-var handlerMethod = function(req,res){
-	res.end("A simple response from the simple web server");
-}
+//var handlerMethod = function(req,res){
+//	res.end("A simple response from the simple web server");
+//}
 
-http.createServer(handlerMethod).listen(3456, 'localhost');
+var app = connect()
+        .use(bodyParser.urlencoded(
+        	{extended:true}	
+        ))
+        .use(function(req,res){  // this defines the handler
+        	var parsedInfo = {};
+        	
+        	parsedInfo.firstName = req.body.userFirstName;
+        	parsedInfo.lastName = req.body.userLastName;
+        	
+        	res.end("User info from parsed form: " + parsedInfo.firstName + " " + parsedInfo.lastName);
+        });
+
+http.createServer(app).listen(3456, 'localhost');
 console.log("HTTP Server listening on port 3456");
