d9e957544304011cc14172e6613dff7399cda4b7
david noakes <david.noakes@gmail.com>
2018-02-20 14:28:30 +1000

    missed these changes

--- DataStoresDemo/MSSQLapp.js
+++ DataStoresDemo/MSSQLapp.js
@@ -16,11 +16,24 @@ var config = {
 
 var callback1 = function(err){
   if(err) console.log(err);	
-  var request = new sql.Request();
+  var request = new mySql.Request();
 	request.query("select * from fd_groups",function(err,recordset){
 		if(err) console.log(err);
 		console.dir(recordset);
 	});
 };
 
+var ps = new mySql.PreparedStatement(connexion);
+var callback2 = function(err,result){
+	if(err) throw err;
+    ps.execute({name:'testfoodgroup',description:'test food group description'},function(err,result){
+       if(err) throw err;
+       console.log(result);
+    })	
+};
+
 var connexion = new mySql.connect(config, callback1);
+
+ps.input('name',mySql.Varchar(50));
+ps.input('description',mySql.Varchar(50));
+ps.prepare('insert into fd_groups (name,description) values (@name,@description)',callback2);
