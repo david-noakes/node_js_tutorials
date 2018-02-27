839d59a2c42d396b3d86a25b52c4e4c422fc1e81
david noakes <david.noakes@gmail.com>
2018-02-27 13:25:43 +1000

    basic express app showing TestPage.html

--- singleExpress/index.js
+++ singleExpress/index.js
@@ -3,9 +3,18 @@
  */
 var express = require('express');
 var app = express();
-app.get('/',function(req, res){
-	res.end("Welcome to a simple app created using the express module")
+
+app.use(express.static(__dirname + '/public'));
+
+//app.get('/',function(req, res){
+//	res.end("Welcome to a simple app created using the express module")
+//});
+app.get('/*',function(req, res){
+	res.writeHead(404);
+	res.end("Resource does not exist.")
 });
+
+
 
 var port = 3456;
 app.listen(port, function(){
