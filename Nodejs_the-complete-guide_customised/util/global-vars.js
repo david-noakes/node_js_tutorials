9874da1aaf612d59825a8a19917d64fd498eea92
david <david.noakes@gmail.com>
2019-10-20 22:25:12 +1000

    remove keys

--- Nodejs_the-complete-guide_customised/util/global-vars.js
+++ Nodejs_the-complete-guide_customised/util/global-vars.js
@@ -2,47 +2,9 @@ const dateFormat = require('dateformat');
 
 var JWT_token;  // holds JWT
 
-var JWT_Secret_Sign = "QhdsRlbwJbdajNxvHRNOpoRFnKs2wOoDU1B8ueENmAY2Y7";
-
 var imageStorePath;
 
-var mongoDBConstants = {
-  MONGO_ATLAS_USER: 'ndj4096-a',
-  MONGO_ATLAS_PW: 'Ndj%24%240519zxq',
-  MONGO_ATLAS_DB_201: '@cluster0-klig6.mongodb.net/angularcourse?retryWrites=true',
-  MONGO_LOCAL_NODEJS_COURSE_DB: 'mongodb://127.0.0.1:27017/nodejscourse',
-  MONGO_LOCAL_ANGULAR_MEAN_STACK_DB: 'mongodb://127.0.0.1:27017/angularMeanStackCourse'
-}
-
 // firebase
-var fbConstants = {
-  fireBaseURL: 'https://angular-training-201.firebaseio.com/',
-  fbRecipeBookURL: 'recipes',
-  fbShoppingListURL: 'shoppingList',
-  fbUserListURL: 'testAuth',
-  fbHeroTableURL: 'heroTable',
-  fbPostsURL: 'posts',
-  zodEmail: 'zod4096@shadowlands.com',
-  zodPassword: 'Ndj$1913',
-  zodUuid: 'QhdsRlbwJbdajNxvHRNOpoRFnKs2',
-  djnEmail: 'david.noakes@gmail.com',
-  djnPassword: 'F$$0719zxq',
-  djnUuid: 'aIffoSyapCTKiTU7HZpDHrCdy173'
-}
-
-const sendGrid = {
-  nodeTraining: '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'
-}
-
-var fbconfig = {
-  apiKey: "AIzaSyC_wOoDU1B8ueENmAY2Y7_JqNZhyz2Bu-0",
-  authDomain: "angular-training-201.firebaseapp.com",
-  databaseURL: "https://angular-training-201.firebaseio.com",
-  projectId: "angular-training-201",
-  storageBucket: "angular-training-201.appspot.com",
-  messagingSenderId: "83927608324"
-};
-var firebaseConnected = false;
 
 // firebase.initializeApp(fbconfig);
   // Get a reference to the database service
@@ -137,11 +99,5 @@ module.exports = {
   getSessionData: getSessionData,
   pullSessionData: pullSessionData,
   putSessionData: putSessionData,
-  JWT_token: JWT_token,
-  JWT_Key: JWT_Secret_Sign,
   imageStorePath: imageStorePath,
-  fbConstants: fbConstants,
-  MONGO_ATLAS_PW: mongoDBConstants.MONGO_ATLAS_PW,
-  MONGO_Config: mongoDBConstants,
-  sendGrid: sendGrid
 }
