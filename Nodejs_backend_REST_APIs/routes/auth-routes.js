00954256272c64e2b08f574a8954a5912b3eec2c
david <david.noakes@gmail.com>
2019-11-07 22:44:22 +1000

    chore: code changes for assignment

--- Nodejs_backend_REST_APIs/routes/auth-routes.js
+++ Nodejs_backend_REST_APIs/routes/auth-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+const { body, check } = require('express-validator');
 
-const User = require('../models/user');
-const authController = require('../controllers/auth');
+const authController = require('../controllers/auth-controller');
+const isAuth = require('../middleware/is-auth');
+const User = require('../models/user-model');
 
 const router = express.Router();
 
@@ -12,18 +13,33 @@ router.put(
     body('email')
       .isEmail()
       .withMessage('Please enter a valid email.')
+      .normalizeEmail({all_lowercase: true})
       .custom((value, { req }) => {
         return User.findOne({ email: value }).then(userDoc => {
           if (userDoc) {
             return Promise.reject('E-Mail address already exists!');
           }
         });
-      })
-      .normalizeEmail(),
-    body('password')
-      .trim()
-      .isLength({ min: 4 }),
-    body('name')
+      }),
+      body(
+        'password',
+        'Please enter a password with at least 4 characters.'
+      )
+      .isLength({ min: 4 })
+      .trim(),
+      body('password')
+      .custom((pValue, { req }) => {
+        const hasNumber = /\d/.test(pValue);
+        const hasUpper = /[A-Z]/.test(pValue);
+        const hasLower = /[a-z]/.test(pValue);
+        const hasSpecial = /[!@#$%^&*()_]/.test(pValue);
+        if (hasNumber && hasSpecial && hasLower && hasUpper) {
+            return true;
+        } else {
+            throw new Error('Passwords require at least one each of number, lowercase, uppercase and special (!@#$%^&*()_) characters.');  
+        };
+      }),
+      body('name')
       .trim()
       .not()
       .isEmpty()
@@ -31,6 +47,33 @@ router.put(
   authController.signup
 );
 
-router.post('/login', authController.login);
+router.post('/login',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .normalizeEmail({all_lowercase: true}),
+    body(
+      'password',
+      'Please enter a password with at least 4 characters.'
+    )
+      .isLength({ min: 4 })
+      .trim(),
+  ],
+  authController.login);
 
-module.exports = router;
+  router.get('/status', isAuth, authController.getUserStatus);
+
+  router.put(
+    '/status',
+    isAuth,
+    [
+      body('status')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('cannot be empty'),
+      ],
+    authController.updateUserStatus
+  );
+  
+  module.exports = router;
