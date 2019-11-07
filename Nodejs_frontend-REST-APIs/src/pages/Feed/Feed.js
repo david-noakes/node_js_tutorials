1cdd84b761b6adfa2a5a4bebd18fe143538b765a
david <david.noakes@gmail.com>
2019-11-07 22:44:53 +1000

    chore: code changes for assignment

--- Nodejs_frontend-REST-APIs/src/pages/Feed/Feed.js
+++ Nodejs_frontend-REST-APIs/src/pages/Feed/Feed.js
@@ -24,7 +24,11 @@ class Feed extends Component {
   perPage = 3;
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -80,7 +84,16 @@ class Feed extends Component {
 
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      method: 'PUT',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        status: this.state.status
+      })
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
